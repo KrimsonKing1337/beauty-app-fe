fe32372460e85e39b133c88634fb56b74110828a
Oleg Shilov <shilov-1@yandex.ru>
2026-04-17 13:18:33 +0300

    refactoring;

--- src/pages/Auth/components/Form/Form.vue
+++ src/pages/Auth/components/Form/Form.vue
@@ -109,29 +109,10 @@ const submitHandler = async () => {
   width: 100%;
 }
 
-.Divider {
-  position: relative;
-  text-align: center;
-
-  &:before {
-    content: '';
-    position: absolute;
-    top: 50%;
-    left: 0;
-    width: 100%;
-    height: 1px;
-    background: var(--border);
-    transform: translateY(-50%);
-  }
-
-  span {
-    position: relative;
-    z-index: 1;
-    display: inline-block;
-    padding: 0 12px;
-    font-size: 14px;
-    color: var(--text-tertiary);
-    background: rgba(255, 255, 255, 0.9);
+@media (max-width: 480px) {
+  .Row {
+    flex-direction: column;
+    align-items: flex-start;
   }
 }
 </style>
--- src/pages/Auth/components/Form/components/Buttons/Buttons.vue
+++ src/pages/Auth/components/Form/components/Buttons/Buttons.vue
@@ -36,5 +36,29 @@ defineProps<Props>();
 </template>
 
 <style scoped lang="scss">
+.Divider {
+  position: relative;
+  text-align: center;
 
+  &:before {
+    content: '';
+    position: absolute;
+    top: 50%;
+    left: 0;
+    width: 100%;
+    height: 1px;
+    background: var(--border);
+    transform: translateY(-50%);
+  }
+
+  span {
+    position: relative;
+    z-index: 1;
+    display: inline-block;
+    padding: 0 12px;
+    font-size: 14px;
+    color: var(--text-tertiary);
+    background: rgba(255, 255, 255, 0.9);
+  }
+}
 </style>
