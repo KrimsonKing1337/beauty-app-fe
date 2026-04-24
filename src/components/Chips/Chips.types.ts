059399014d57590ce32d441413bfdd3d2cf4ef82
Oleg Shilov <shilov-1@yandex.ru>
2026-04-24 13:46:01 +0300

    removed unused components;

--- src/components/Chips/Chips.types.ts
+++
@@ -1,9 +0,0 @@
-type Chip = {
-  id: string;
-  label: string;
-  isActive?: boolean;
-};
-
-export type ChipsProps = {
-  chips: Chip[];
-};
--- src/components/Chips/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as Chips } from './Chips.vue';
-export * from './Chips.types.ts';
